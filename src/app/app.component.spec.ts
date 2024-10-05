87484791f70af91f070cf4c657ec2b8d85f85e3d
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-10-05 16:22:00 -0300

    Adicionada máscara para os campos de data

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-web1'`, () => {
+  it(`should have as title 'crud-web2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-web1');
+    expect(app.title).toEqual('crud-web2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crud-web1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crud-web2');
   });
 });
